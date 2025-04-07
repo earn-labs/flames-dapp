a650f6e898481fd1bdb3cd5f9867386b002ea8d5
trashpirate <nadinaoates@gmail.com>
2025-04-07 11:33:16 -0500

    change rarible to opensea

--- frontend/src/components/navigation/navbar.tsx
+++ frontend/src/components/navigation/navbar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 type Props = {};
 
-export default function Navbar({}: Props) {
+export default function Navbar({ }: Props) {
   return (
     <nav className="mx-auto mb-8 mt-3 flex justify-between gap-5 align-middle md:w-full">
       <div className="my-auto h-fit w-fit flex-row rounded-md border-2 border-white bg-white font-bold text-black hover:bg-slate-400 sm:w-36 sm:justify-between">
@@ -26,18 +26,18 @@ export default function Navbar({}: Props) {
       <div className="my-auto h-fit w-fit flex-row rounded-md border-2 border-white bg-white font-bold text-black hover:bg-slate-400 sm:w-40 sm:justify-between">
         <a
           className="pointer-events-auto mx-auto flex h-10 items-center align-middle text-lg uppercase sm:gap-1 sm:text-center lg:p-0 "
-          href="https://rarible.com/collection/0x12a961e8cc6c94ffd0ac08deb9cde798739cf775/items"
+          href="https://opensea.io/collection/earnflames"
           rel="noopener noreferrer"
         >
           <Image
-            src="/rarible.png"
-            alt="Rarible logo"
+            src="/opensea.png"
+            alt="Opensea logo"
             className="mx-1 h-8 w-auto overflow-hidden rounded-md"
             width={40}
             height={40}
             priority
           />
-          <div className="w-0 scale-0 sm:w-fit sm:scale-100">Rarible</div>
+          <div className="w-0 scale-0 sm:w-fit sm:scale-100">Opensea</div>
         </a>
       </div>
       <div className="my-auto h-fit w-fit flex-row rounded-md border-2 border-white bg-white font-bold text-black hover:bg-slate-400 sm:w-44 sm:justify-between">
